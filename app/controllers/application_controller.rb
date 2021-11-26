d875a3e4e8f0be3bae5003d4c46fe0c784d0dcc8
helenavocado <hwang92@uchicago.edu>
2021-11-26 14:54:32 +0000

    finished sign up

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,7 @@
 class ApplicationController < ActionController::Base
+  def load_current_user
+    the_id = session[:user_id]
+    @current_user = User.where({ :id => the_id }).first
+  end
   
 end
