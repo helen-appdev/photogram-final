e3adfd6e7e7340392a9656655c1e1c94cfecdf65
helenavocado <hwang92@uchicago.edu>
2021-11-27 02:01:31 +0000

    dlkjfa

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,4 +1,7 @@
 class ApplicationController < ActionController::Base
+  
+  before_action(:load_current_user)
+  
   def load_current_user
     the_id = session[:user_id]
     @current_user = User.where({ :id => the_id }).first
